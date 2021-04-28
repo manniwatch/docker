463eaaf7836ed7f80bd1d06ff7722852cb112cc2
DonMahallem <donmahallem@users.noreply.github.com>
2021-04-28 12:11:00 +0200

    feat: add SIGINT handling for docker

--- src/index.ts
+++ src/index.ts
@@ -6,10 +6,22 @@ import { ManniWatchProxyServer } from '@manniwatch/api-proxy-server';
 import { Config } from './config';
 
 const server: ManniWatchProxyServer = new ManniWatchProxyServer(Config.endpoint, Config.port);
-server.start();
+server.start()
+    .then((): void => {
+        // tslint:disable-next-line:no-console
+        console.info(`Server started on ${Config.port} with endpoint ${Config.endpoint}`);
+    });
 process.on('SIGINT', (): void => {
     // tslint:disable-next-line:no-console
     console.info('Interrupted');
-    server.stop();
-    process.exit(0);
+    server.stop()
+        .then((): void => {
+            console.log('Server closed');
+        })
+        .catch((err: any): void => {
+            console.error('Error occured while stoping', err);
+        })
+        .finally((): void => {
+            process.exit(0);
+        });
 });
