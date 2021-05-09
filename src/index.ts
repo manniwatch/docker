6375d4f5ca6036a4c8130a13e3dcaf14adcdbf14
DonMahallem <donmahallem@users.noreply.github.com>
2021-05-09 10:30:50 +0200

    feat: use ManniWatchApiProxyServer

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,10 @@
  * Source https://github.com/manniwatch/docker
  */
 
-import { ManniWatchProxyServer } from '@manniwatch/api-proxy-server';
+import { ManniWatchApiProxyServer } from '@manniwatch/api-proxy-server';
 import { Config } from './config';
 
-const server: ManniWatchProxyServer = new ManniWatchProxyServer(Config.endpoint, Config.port);
+const server: ManniWatchApiProxyServer = new ManniWatchApiProxyServer(Config.endpoint, Config.port);
 server.start()
     .then((): void => {
         // tslint:disable-next-line:no-console
